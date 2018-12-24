0d7d821c5dc4d315ff955b477e99797b75030064
Daniël te Winkel <github@twia.nl>
2018-12-24 14:37:54 +0100

    Added first unit tests.

---
+++ Twia.AzureFunction.SimpleInjector.UnitTests/Config/DefaultStartConfigurationTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Twia.AzureFunction.SimpleInjector.Config;
+
+namespace Twia.AzureFunction.SimpleInjector.UnitTests.Config
+{
+    [TestClass]
+    public class DefaultStartConfigurationTests
+    {
+        [TestMethod]
+        public void AddILogger_ReturnsExpectedDefault()
+        {
+            var sut = new DefaultStartConfiguration();
+
+            sut.AddILogger.Should().BeTrue("The default value is 'true'.");
+        }
+
+        [TestMethod]
+        public void AddILoggerOfT_ReturnsExpectedDefault()
+        {
+            var sut = new DefaultStartConfiguration();
+
+            sut.AddILoggerOfT.Should().BeFalse("The default value is 'false'.");
+        }
+
+    }
+}
