332979bed4d4ebb062349aea6e7a6fe8326c25b7
Daniël te Winkel <bitbucket@te-winkel.nl>
2019-06-10 12:01:41 +0200

    Added parameter unit tests.

--- Twia.AzureFunction.SimpleInjector.UnitTests/SimpleInjectorConfigurationExtensionsTests.cs
+++ Twia.AzureFunction.SimpleInjector.UnitTests/SimpleInjectorConfigurationExtensionsTests.cs
@@ -9,8 +9,10 @@ namespace Twia.AzureFunction.SimpleInjector.UnitTests
     [TestFixture]
     public class SimpleInjectorConfigurationExtensionsTests : ConfigurationTestBase
     {
+        private const string _sectionName = "mySection";
+
         [Test]
-        public void RegisterSettingsSingleton_WithNullForContainer_ThrowsException()
+        public void RegisterSettingsSingleton_ConcreteNoSection_WithNullForContainer_ThrowsException()
         {
             var configuration = Mock.Of<IConfiguration>();
 
@@ -20,13 +22,123 @@ namespace Twia.AzureFunction.SimpleInjector.UnitTests
         }
 
         [Test]
-        public void RegisterSettingsSingleton_WithNullForConfiguration_ThrowsException()
+        public void RegisterSettingsSingleton_ConcreteNoSection_WithNullForConfiguration_ThrowsException()
         {
             var container = new Container();
 
             var exception = Assert.Throws<ArgumentNullException>(() => container.RegisterSettingsSingleton<DummySettings>(null));
 
             Assert.That(exception.ParamName, Is.EqualTo("configuration"));
+        }
+
+        [Test]
+        public void RegisterSettingsSingleton_ConcreteSection_WithNullForContainer_ThrowsException()
+        {
+            var configuration = Mock.Of<IConfiguration>();
+
+            var exception = Assert.Throws<ArgumentNullException>(() => SimpleInjectorConfigurationExtensions.RegisterSettingsSingleton<DummySettings>(null, configuration, _sectionName));
+
+            Assert.That(exception.ParamName, Is.EqualTo("container"));
+        }
+
+        [Test]
+        public void RegisterSettingsSingleton_ConcreteSection_WithNullForConfiguration_ThrowsException()
+        {
+            var container = new Container();
+
+            var exception = Assert.Throws<ArgumentNullException>(() => container.RegisterSettingsSingleton<DummySettings>(null, _sectionName));
+
+            Assert.That(exception.ParamName, Is.EqualTo("configuration"));
+        }
+
+        [Test]
+        public void RegisterSettingsSingleton_ConcreteSection_WithNullForSectionName_ThrowsException()
+        {
+            var configuration = Mock.Of<IConfiguration>();
+            var container = new Container();
+
+            var exception = Assert.Throws<ArgumentNullException>(() => container.RegisterSettingsSingleton<DummySettings>(configuration, null));
+
+            Assert.That(exception.ParamName, Is.EqualTo("sectionName"));
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("\t")]
+        public void RegisterSettingsSingleton_ConcreteSection_WithEmptySectionName_ThrowsException(string sectionName)
+        {
+            var configuration = Mock.Of<IConfiguration>();
+            var container = new Container();
+
+            var exception = Assert.Throws<ArgumentException>(() => container.RegisterSettingsSingleton<DummySettings>(configuration, sectionName));
+
+            Assert.That(exception.ParamName, Is.EqualTo("sectionName"));
+        }
+
+        [Test]
+        public void RegisterSettingsSingleton_InterfaceNoSection_WithNullForContainer_ThrowsException()
+        {
+            var configuration = Mock.Of<IConfiguration>();
+
+            var exception = Assert.Throws<ArgumentNullException>(() => SimpleInjectorConfigurationExtensions.RegisterSettingsSingleton<IDummySettings, DummySettings>(null, configuration));
+
+            Assert.That(exception.ParamName, Is.EqualTo("container"));
+        }
+
+        [Test]
+        public void RegisterSettingsSingleton_InterfaceNoSection_WithNullForConfiguration_ThrowsException()
+        {
+            var container = new Container();
+
+            var exception = Assert.Throws<ArgumentNullException>(() => container.RegisterSettingsSingleton<IDummySettings, DummySettings>(null));
+
+            Assert.That(exception.ParamName, Is.EqualTo("configuration"));
+        }
+
+        [Test]
+        public void RegisterSettingsSingleton_InterfaceSection_WithNullForContainer_ThrowsException()
+        {
+            var configuration = Mock.Of<IConfiguration>();
+
+            var exception = Assert.Throws<ArgumentNullException>(() => SimpleInjectorConfigurationExtensions.RegisterSettingsSingleton<IDummySettings, DummySettings>(null, configuration, _sectionName));
+
+            Assert.That(exception.ParamName, Is.EqualTo("container"));
+        }
+
+        [Test]
+        public void RegisterSettingsSingleton_InterfaceSection_WithNullForConfiguration_ThrowsException()
+        {
+            var container = new Container();
+
+            var exception = Assert.Throws<ArgumentNullException>(() => container.RegisterSettingsSingleton<IDummySettings, DummySettings>(null, _sectionName));
+
+            Assert.That(exception.ParamName, Is.EqualTo("configuration"));
+        }
+
+        [Test]
+        public void RegisterSettingsSingleton_InterfaceSection_WithNullForSectionName_ThrowsException()
+        {
+            var configuration = Mock.Of<IConfiguration>();
+            var container = new Container();
+
+            var exception = Assert.Throws<ArgumentNullException>(() => container.RegisterSettingsSingleton<IDummySettings, DummySettings>(configuration, null));
+
+            Assert.That(exception.ParamName, Is.EqualTo("sectionName"));
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("\t")]
+        public void RegisterSettingsSingleton_InterfaceSection_WithEmptySectionName_ThrowsException(string sectionName)
+        {
+            var configuration = Mock.Of<IConfiguration>();
+            var container = new Container();
+
+            var exception = Assert.Throws<ArgumentException>(() => container.RegisterSettingsSingleton<IDummySettings, DummySettings>(configuration, sectionName));
+
+            Assert.That(exception.ParamName, Is.EqualTo("sectionName"));
         }
     }
 }
--- Twia.AzureFunction.SimpleInjector.UnitTests/SimpleInjectorCrossWireExtensionsTests.cs
+++ Twia.AzureFunction.SimpleInjector.UnitTests/SimpleInjectorCrossWireExtensionsTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Moq;
